8c82b1e2f354fc04db4a9cb08188250b368b4aac
gpanagiotidis <p-ioannis@hotmail.com>
2015-07-29 16:06:28 +0300

    Update Examples.java

--- src/main/java/gr/james/socialinfluence/main/Examples.java
+++ src/main/java/gr/james/socialinfluence/main/Examples.java
@@ -6,13 +6,67 @@ import gr.james.socialinfluence.game.GameDefinition;
 import gr.james.socialinfluence.game.Player;
 import gr.james.socialinfluence.game.players.BruteForcePlayer;
 import gr.james.socialinfluence.graph.MemoryGraph;
+import gr.james.socialinfluence.graph.Vertex;
+import gr.james.socialinfluence.graph.io.Dot;
+import gr.james.socialinfluence.util.RandomHelper;
 
 import java.io.IOException;
+import java.util.Set;
 
 public class Examples {
     public static void main(String[] args) throws IOException {
-        Graph g = new CycleGenerator<>(MemoryGraph.class, 3).create();
-        Player p = new BruteForcePlayer();
-        p.getMove(g, new GameDefinition(3, 3.0, 0));
+        Graph g = new MemoryGraph();
+        int n = 50;
+        int k = 4;
+        double p = 0.1;
+
+        g.addVertices(n);
+        Vertex sub = g.getRandomVertex();
+
+        System.out.println(g.getVerticesCount());
+
+        for (int i=0; i<n; i++){
+            for (int j=1; j<=k/2; j++){
+                if (i+j<n) {
+                    g.addEdge(g.getVertexFromIndex(i), g.getVertexFromIndex(i + j), true);
+                }
+                else{
+                    g.addEdge(g.getVertexFromIndex(i), g.getVertexFromIndex(i + j - n), true);
+                }
+            }
+        }
+
+        for (int i=0; i<n; i++){
+            for (int j=1; j<=k/2; j++){
+                if (i+j<n) {
+                    if (p >= RandomHelper.getRandom().nextDouble()) {
+                        g.removeEdge(g.getVertexFromIndex(i), g.getVertexFromIndex(i + j));
+                        g.removeEdge(g.getVertexFromIndex(i + j), g.getVertexFromIndex(i));
+                        sub = g.getRandomVertex();
+                        while (sub == g.getVertexFromIndex(i) || sub == g.getVertexFromIndex(i + j) || g.containsEdge(g.getVertexFromIndex(i), sub) || g.containsEdge(sub, g.getVertexFromIndex(i))) {
+                            sub = g.getRandomVertex();
+                        }
+                        g.addEdge(g.getVertexFromIndex(i), sub, true);
+                    }
+                }
+                else {
+                    if(p>=RandomHelper.getRandom().nextDouble()){
+                        g.removeEdge(g.getVertexFromIndex(i), g.getVertexFromIndex(i + j - n));
+                        g.removeEdge(g.getVertexFromIndex(i + j - n), g.getVertexFromIndex(i));
+                        sub=g.getRandomVertex();
+                        while (sub==g.getVertexFromIndex(i) || sub==g.getVertexFromIndex(i + j - n) || g.containsEdge(g.getVertexFromIndex(i), sub) || g.containsEdge(sub, g.getVertexFromIndex(i))){
+                            sub=g.getRandomVertex();
+                        }
+                        g.addEdge(g.getVertexFromIndex(i), sub, true);
+                    }
+                }
+            }
+        }
+
+
+        System.out.println(g.getEdgesCount());
+
+        Dot dot = new Dot();
+            dot.to(g, System.out);
     }
 }
